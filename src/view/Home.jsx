7a5ee39b722dc1c993fc2a2f867bddd0e7f6ba77
Jhon peralta <jhonperalta13@gmail.com>
2023-12-05 18:31:09 -0500

    cambios en la frese del centro  de homeview

--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -2,21 +2,21 @@ import './csshome.css';
 
 export const Home = () => {
     return (
-       <>
-         <section className="background-radial-gradient-d d-flex align-items-center justify-content-center overflow-lg-hidden ">
-             <div className="container-fluid ">
-                 <div className="container_formulario_request-d justify-content-center  ">
-                     <div className="text_info d-none d-lg-block">
-                         <h1 className="my-5 display-7 fw-bold ls-tight">
-                             CRM para Centros Comerciales <br />
-                         </h1>
-                         <p className="mb-4 opacity-70">
-                             {/* Texto opcional */}
-                         </p>
-                     </div>
-                 </div>
-             </div>
-         </section>
-       </>
+        <>
+            <section className="background-radial-gradient-d d-flex align-items-center justify-content-center overflow-lg-hidden ">
+                <div className="container-fluid ">
+                    <div className="container_formulario_request-d justify-content-center  ">
+                        <div className="text_info d-none d-lg-block">
+                            <h1 className="my-5 display-7 fw-bold ls-tight">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, repudiandae earum. Aliquid totam mollitia itaque est nihil? Tempore, autem neque. Eveniet temporibus ab ex natus repellat provident pariatur sapiente voluptas.      CRM para Centros Comerciales <br />
+                            </h1>
+                            <p className="mb-4 opacity-70">
+                                {/* Texto opcional */}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
     )
- }
+}
